42d1b2fb1577be4fc1cc938aef6df9d550b046d8
Andrei Kolesnikovich <andrei.kolesnikovich@xplanis.com>
2025-02-16 09:37:46 +0100

    * Init angular project * Init 2 components * Use components in app

---
+++ angular-project/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+//import { RouterOutlet } from '@angular/router';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { MoreInfoButtonComponent } from './more-info-button/more-info-button.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [UserInfoComponent, MoreInfoButtonComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'angular-project';
+}
